9d14c44a731caa61355dac128c8da7dd833c014a
Vishruth <harithsa@outlook.com>
2016-06-12 23:40:00 +0530

    Mailiong Changes and Version Enhancement

--- include/sendemail.php
+++ include/sendemail.php
@@ -1,51 +1,17 @@
-<?php
-
-require_once('phpmailer/class.phpmailer.php');
-
-$mail = new PHPMailer();
-
-if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-    if( $_POST['contactform-name'] != '' AND $_POST['contactform-email'] != '' AND $_POST['contactform-message'] != '' ) {
-
-        $name = $_POST['cf-name'];
-		$email = $_POST['cf-email'];
-		$phone = $_POST['cf-phone'];
-		$subject = $_POST['cf-subject'];
-		$message = $_POST['cf-message'];
-
-        $subject = isset($subject) ? $subject : 'New Message From Contact Form'; // Enter the subject here.
-
-        $toemail = 'support@inspiredmemories.in'; // Enter your mail addres here.
-        $toname = 'Support Team'; // Enter your name here.
-
-		$mail->SetFrom( $email , $name );
-		$mail->AddReplyTo( $email , $name );
-		$mail->AddAddress( $toemail , $toname );
-		$mail->Subject = $subject;
-
-		$name = isset($name) ? "Name: $name<br><br>" : '';
-		$email = isset($email) ? "Email: $email<br><br>" : '';
-		$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
-		$message = isset($message) ? "Message: $message<br><br>" : '';
-
-		$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
-
-		$body = "$name $email $phone $message $referrer";
-
-		$mail->MsgHTML( $body );
-		$sendEmail = $mail->Send();
-
-		if( $sendEmail == true ):
-			echo 'Message sent!';
-		else:
-			echo 'Email could not be sent. Please Try Again later.<br /><br />Reason:<br />' . $mail->ErrorInfo . '';
-		endif;
-
-    } else {
-        echo 'Please Fill up all the Fields and Try Again.';
-    }
-} else {
-    echo 'An unexpected error occured. Please Try Again later.';
-}
-
-?>
+<?php 
+	
+	$name = $_POST[ 'contact-name' ];
+	$email = $_POST[ 'contact-email' ];
+	$subject = $_POST[ 'contact-subject' ];
+	$message = $_POST[ 'contact-message' ];
+	$mail_subject = $subject . "(Visitor Contact)";
+	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message."";
+	
+	/* Replace YOUR_MAIL With Your Mail Address inside '' */
+	if ( mail( 'support@inspiredmemories.in', $mail_subject, $message, "From:" . $email ) ) {
+		echo "Thank you <strong>$name</strong> for contacting with us !!!";
+	} else {
+		echo "Error in sending message !!! Please try again";
+	}
+	
+ ?>
